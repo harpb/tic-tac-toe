b96c77d2074192da5787986db8cb65a33bebb335
Harp. B <hi@harpb.com>
2015-04-24 19:05:37 -0700

    from django.conf import settings

--- webapp/webapp/middlewares.py
+++ webapp/webapp/middlewares.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 
 class HostMiddleware(object):
 
     def process_request(self, request):
         print 'host = request.META', request.META.get('HTTP_HOST'), request.META.get('HTTP_X_FORWARDED_HOST')
+        print 'settings.ALLOWED_HOSTS', settings.ALLOWED_HOSTS
         try:
             print 'host', request.get_host()
         except Exception, e:
